cb850f2d24424aed3bba1c773daf7d81a94cc31c
nulla <40925101+nulla-git@users.noreply.github.com>
2022-01-15 03:37:18 -0600

    updated ~/minecraft_server directories to ~/fabric-server.

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 apt update -qq && apt install -qq --yes openjdk-17-jdk nano
 apt uprade -qq --yes
 cd ../
-mkdir minecraft_server
-cd minecraft_server
+mkdir fabric_server
+cd fabric_server
 echo ----------------------------------------
 echo The server jar will now be downloaded...
 echo ----------------------------------------
@@ -27,22 +27,26 @@ cd ../
 mkdir fabric_launcher
 cd fabric_launcher
 echo -------------------------------------------------------------------------------------------------
-echo The fabric installer will now be downloaded, install the server in ~/minecraft_server.
+echo The fabric installer will now be downloaded, install the server in ~/fabric_server.
 echo When done, close the launcher and DO NOT GENERATE LAUNCH SCRIPTS, THIS SCRIPT WILL DO IT FOR YOU.
 echo -------------------------------------------------------------------------------------------------
 sleep 10s
 wget https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.10.2/fabric-installer-0.10.2.jar
 java -jar fabric-installer-0.10.2.jar
+echo --------------------------------------------------------
+echo Removing fabric_launcher and generating launch script...
+echo --------------------------------------------------------
 cd ../
-cd minecraft_server
+cd fabric_server
 touch start.sh
 echo java -Xmx5G -jar fabric-server-launch.jar >> start.sh
+
 cd ../
 cd fabric_launcher
 rm fabric-installer-0.10.2.jar
 cd ../
 rmdir fabric_launcher
 echo -------------------------------------------------------------------------------------------------------------------
-echo The server has been installed in the ~/minecraft_server directory.
-echo Forward ports, then install the mods you want. Afterwards, run the start.sh file in ~/minecraft_server and have fun!
+echo The server has been installed in the ~/fabric_server directory.
+echo Forward ports, then install the mods you want. Afterwards, run the start.sh file in ~/fabric_server and have fun!
 echo -------------------------------------------------------------------------------------------------------------------
