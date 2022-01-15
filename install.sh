bfc0e2bf884aa2875a3bc3e373d9b5d1731a7de1
nulla <40925101+nulla-git@users.noreply.github.com>
2022-01-14 23:24:37 -0600

    warning not to generate scripts with fabric.

--- install.sh
+++ install.sh
@@ -21,14 +21,16 @@ sleep 1s
 cd ../
 mkdir fabric_launcher
 cd fabric_launcher
-echo The fabric installer will now be downloaded, install the server in ~/minecraft_server
-sleep 7s
+echo The fabric installer will now be downloaded, install the server in ~/minecraft_server.
+sleep 3s
+echo When done, close the launcher and DO NOT GENERATE LAUNCH SCRIPTS, THIS SCRIPT WILL DO IT FOR YOU.
+sleep 15s
 wget https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.10.2/fabric-installer-0.10.2.jar
 java -jar fabric-installer-0.10.2.jar
 sleep 2s
 cd ../
 cd minecraft_server
 touch start.sh
-echo java -Xmx3600m -jar fabric-server-launch.jar >> start.sh
+echo java -Xmx5G -jar fabric-server-launch.jar >> start.sh
 sleep 1s
 echo The server is now set up, run it and have fun!
