cd91e90915844c2fb33af58c7a173ac73465465f
nulla <40925101+nulla-git@users.noreply.github.com>
2022-01-14 23:45:45 -0600

    made the script more user-friendly.

--- install.sh
+++ install.sh
@@ -1,36 +1,45 @@
+function pause(){
+   read -rsp "$*"
+}
+
 apt update -qq && apt install -qq --yes openjdk-17-jdk nano
 apt uprade -qq --yes
-sleep 1s
 cd ../
 mkdir minecraft_server
 cd minecraft_server
-sleep 1s
-echo the server jar will now be downloaded...
-sleep 5s
+echo ----------------------------------------
+echo The server jar will now be downloaded...
+echo ----------------------------------------
 wget https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar
 
-echo you will now enter the server environment to generate the eula.txt
-sleep 5s
+echo You will now enter the server environment to generate the eula.txt
+pause 'Press enter to continue, or CTRL+C to exit.'
 java -jar server.jar
 
-echo because I am lazy, you will now be dropped into nano to accept the eula
-sleep 5s
+echo You will now be put into nano to accept the EULA. Read it here: https://www.minecraft.net/en-us/eula
+pause 'Press enter to continue, or CTRL+C to exit.'
 nano eula.txt
-sleep 1s
 
 cd ../
 mkdir fabric_launcher
 cd fabric_launcher
+echo -------------------------------------------------------------------------------------------------
 echo The fabric installer will now be downloaded, install the server in ~/minecraft_server.
-sleep 3s
 echo When done, close the launcher and DO NOT GENERATE LAUNCH SCRIPTS, THIS SCRIPT WILL DO IT FOR YOU.
-sleep 15s
+echo -------------------------------------------------------------------------------------------------
+pause 'Press enter to continue, or CTRL+C to exit.'
 wget https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.10.2/fabric-installer-0.10.2.jar
 java -jar fabric-installer-0.10.2.jar
-sleep 2s
 cd ../
 cd minecraft_server
 touch start.sh
 echo java -Xmx5G -jar fabric-server-launch.jar >> start.sh
-sleep 1s
-echo The server is now set up, run it and have fun!
+cd ../
+cd fabric_launcher
+rm fabric-installer-0.10.2.jar
+cd ../
+rmdir fabric_launcher
+echo -------------------------------------------------------------------------------------------------------------------
+echo The server has been installed in the ~/minecraft_server directory.
+echo Forward ports, then install the mods you want. Afterwards, run the start.sh file in ~/minecraft_server and have fun!
+echo -------------------------------------------------------------------------------------------------------------------
