814ce12afea84fab76cf2fa05604eaabcfe43234
nulla <40925101+nulla-git@users.noreply.github.com>
2022-01-14 21:47:14 -0600

    install server 2:electric boogalo

--- install.sh
+++ install.sh
@@ -1,8 +1,33 @@
-apt update -qq && apt install -qq --yes openjdk-17-jdk git tar
+apt update -qq && apt install -qq --yes openjdk-17-jdk nano
+apt uprade -qq --yes
+sleep 1s
+mkdir minecraft_server
+cd minecraft_server
+sleep 1s
+echo the server jar will now be downloaded...
+sleep 5s
+wget https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar
 
-git clone https://github.com/nulla-git/Minecraft_Server_18.1.git
-cd Minecraft_Server_18.1
-tar -xpvf server.tar.gz
-rm server.tar.gz
+echo you will now enter the server environment to generate the eula.txt
+sleep 10s
+java -jar server.jar
 
-sh start.sh
+echo because I am lazy, you will now be dropped into nano to accept the eula
+sleep 15s
+nano eula.txt
+sleep 1s
+
+cd ../
+mkdir fabric_launcher
+cd fabric_launcher
+echo The fabric installer will now be downloaded, install the server in ~/minecraft_server
+sleep 20s
+wget https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.10.2/fabric-installer-0.10.2.jar
+java -jar fabric-installer-0.10.2.jar
+sleep 2s
+cd ../
+cd minecraft_server
+touch start.sh
+echo java -Xmx5g -jar fabric-server-launch.jar
+sleep 1s
+echo The server is now set up, run it and have fun!
